27d46695bdd3910bc7b307e65a7d5d0490be6793
Marcel Ehret <stoezio@gmail.com>
2026-01-03 15:18:34 +0100

    Make the book to stat with the first argument of the main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 
+import sys
+
 from stats import get_num_words, get_char_count, get_report
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-def main():
-    book = "books/frankenstein.txt"
+def usage():
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+def main(book):
     num_words = get_num_words(get_book_text(book))
+    char_count = get_char_count(get_book_text(book))
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
 
-    char_count = get_char_count(get_book_text("books/frankenstein.txt"))
     for item in get_report(char_count):
         print(f"{item['char']}: {item['num']}")
 
     print("============= END ===============")
 
-main()
+
+print(sys.argv)
+if len(sys.argv) == 2:
+    main(sys.argv[1])
+else:
+    usage()
